2ee6ec01ee29b5ab4894c03e09aea6977b382b24
YohanDan <techwear006@gmail.com>
2026-04-22 14:48:53 -0500

    feature(negocio): Terminamos clase CategoriaControl.java

--- src/negocio/CategoriaControl.java
+++ src/negocio/CategoriaControl.java
@@ -12,20 +12,25 @@ public class CategoriaControl {
     private final CategoriaDAO DATOS;
     private Categoria obj;
     private DefaultTableModel modeloTabla;
+    public int registroMostrado;
     
     public CategoriaControl(){
         this.DATOS= new CategoriaDAO();
         this.obj = new Categoria();
+        this.registroMostrado = 0;
     }
     
     public DefaultTableModel listar(String texto){
         
         List<Categoria> lista = new ArrayList<>();
         lista.addAll(DATOS.listar(texto));
+        
         String[] titulos = {"Id", "Nombre", "Descripción", "Estado"};
         this.modeloTabla=new DefaultTableModel(null, titulos);
+        
         String estado;
         String[] registro = new String[4]; 
+        this.registroMostrado = 0;
         
         for(Categoria item:lista){
             if(item.isActivo()){
@@ -38,6 +43,7 @@ public class CategoriaControl {
             registro[2] = item.getDescripcion();
             registro[3] = estado;
             this.modeloTabla.addRow(registro);
+            this.registroMostrado = this.registroMostrado + 1;
         }
         return this.modeloTabla;
     }
@@ -57,12 +63,46 @@ public class CategoriaControl {
         }
     }
     public String actualizar(int id, String nombre, String nombreAnt, String descripcion){
+        if(nombre.equals(nombreAnt)){
+            obj.setId(id);
+            obj.setNombre(nombre);
+            obj.setDescripcion(descripcion);
+            if (DATOS.actualizar(obj)) {
+                return "OK";
+            }else{
+                return "Error en la actualizacion";
+            }
+        }else {
+            if (DATOS.existe(nombre)) {
+                return "El registro ya existe";
+            }else {
+                obj.setId(id);
+                obj.setNombre(nombre);
+                obj.setDescripcion(descripcion);
+                if (DATOS.actualizar(obj)) {
+                    return "OK";
+                }else{
+                    return "Error en la actualizacion";
+                }
+            }
+        }
     }
     public String descativar(int id){
+        if (DATOS.desactivar(id)) {
+            return "OK";
+        }else{
+            return "No se puede desactivar el registro";
+        }
     }
     public String activar(int id){
+        if (DATOS.activar(id)) {
+            return "OK";
+        }else{
+            return "No se puede activar el registro";
+        }
     }
     public int total(){
+        return DATOS.total();
     }
-
+    
 }
